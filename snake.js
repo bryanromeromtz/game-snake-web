926fbb0731cd62fcf8dbe671ea2c13a5b3797d72
bryanromeromtz <cryptomtz@hotmail.com>
2021-01-12 15:04:08 -0600

    Escribimos código para mover culebras de cualquier tamaño

--- snake.js
+++ snake.js
@@ -1,8 +1,38 @@
+let DIRECCIONES = {
+  ARRIBA: 1,
+  ABAJO: 2,
+  IZQUIERDA: 3,
+  DERECHA: 4
+};
+
 let juegoCanvas = document.getElementById("juegoCanvas");
 let ctx = juegoCanvas.getContext("2d");
 
-let culebraPosX = 0;
-let culebraPosY = 0;
+let culebra = [
+  { posX: 20, posY: 0 },
+  { posX: 20, posY: 20 },
+  { posX: 40, posY: 20 },
+  { posX: 60, posY: 20 },
+]
+
+function moverCulebra (nuevaDireccion, culebra) {
+  let cabezaPosX = culebra[0].posX;
+  let cabezaPosY = culebra[0].posY;
+  
+  if (nuevaDireccion === DIRECCIONES.DERECHA) {
+    cabezaPosX += 20;
+  } else if (nuevaDireccion === DIRECCIONES.IZQUIERDA) {
+    cabezaPosX -= 20;
+  } else if (nuevaDireccion === DIRECCIONES.ABAJO) {
+    cabezaPosY += 20;
+  } else if (nuevaDireccion === DIRECCIONES.ARRIBA) {
+    cabezaPosY -= 20;
+  }
+  // Agregamos la nueva cabeza al principio de la lista.
+  culebra.unshift({posX: cabezaPosX,posY: cabezaPosY});
+  // Borramos la cola de la culebra.
+  culebra.pop();
+}
 
 function dibujarCuadricula(context) {
   for (let x = 20; x <= 600; x += 20) {
@@ -12,7 +42,7 @@ function dibujarCuadricula(context) {
     context.lineTo(x, 600);
     context.stroke();
   }
-  
+
   for (let y = 20; y <= 600; y += 20) {
     context.beginPath();
     context.fillStyle = "black";
@@ -23,34 +53,41 @@ function dibujarCuadricula(context) {
 }
 
 
-function dibujarCulebra(context, posX, posY) {
-  context.beginPath();
-  context.fillStyle = "black";
-  context.fillRect(posX, posY, 20, 20);
-  context.stroke();
+function dibujarCulebra(context, culebra) {
+  
+  for (let i = 0; i < culebra.length; i++) {
+    context.beginPath();
+    context.fillStyle = "black";
+    context.fillRect(culebra[i].posX, culebra[i].posY, 20, 20);
+    context.stroke();
+  }
 }
 
 dibujarCuadricula(ctx);
 
-dibujarCulebra(ctx, culebraPosX, culebraPosY);
+dibujarCulebra(ctx, culebra);
 
 
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
+  let nuevaDireccion;
   if (e.code === "ArrowUp") {
-    culebraPosY -= 20;
+    nuevaDireccion = DIRECCIONES.ARRIBA;
   } else if (e.code === "ArrowDown") {
-    culebraPosY += 20;
+    nuevaDireccion = DIRECCIONES.ABAJO;
   } else if (e.code === "ArrowLeft") {
-    culebraPosX -= 20;
+    nuevaDireccion = DIRECCIONES.IZQUIERDA;
   } else if (e.code === "ArrowRight") {
-    culebraPosX += 20;
+    nuevaDireccion = DIRECCIONES.DERECHA;
   } else {
     return;
   }
+
+  moverCulebra(nuevaDireccion, culebra);
+
   ctx.clearRect(0, 0, 600, 600);
   dibujarCuadricula(ctx);
-  dibujarCulebra(ctx, culebraPosX, culebraPosY);
-  
+  dibujarCulebra(ctx, culebra);
+
 
 });
 
