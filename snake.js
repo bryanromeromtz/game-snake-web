ec49448e437cec94568dca518f223757bbd43888
bryanromeromtz <cryptomtz@hotmail.com>
2021-01-12 02:00:25 -0600

    Envolvemos la lógica para dibujar a la culebra y la cuadrícula en funciones

--- snake.js
+++ snake.js
@@ -1,35 +1,36 @@
 let juegoCanvas = document.getElementById("juegoCanvas");
 let ctx = juegoCanvas.getContext("2d");
 
-
-/*
-ctx.beginPath();
-ctx.fillStyle = "orange";
-ctx.fillRect(0, 0, 100, 100);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.fillStyle = "blue";
-ctx.ellipse(300, 300, 50, 50, 0, 0, 2 * Math.PI);
-ctx.fill();
-ctx.stroke();
-
-ctx.font = "20 px Arial";
-ctx.fillStyle = "black";
-ctx.fillText("SNAKE!", 200, 200);
-*/
-for (let x = 20; x <= 600; x += 20) {
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.moveTo(x, 0);
-  ctx.lineTo(x, 600);
-  ctx.stroke();
+function dibujarCuadricula(context) {
+  for (let x = 20; x <= 600; x += 20) {
+    context.beginPath();
+    context.fillStyle = "black";
+    context.moveTo(x, 0);
+    context.lineTo(x, 600);
+    context.stroke();
+  }
+  
+  for (let y = 20; y <= 600; y += 20) {
+    context.beginPath();
+    context.fillStyle = "black";
+    context.moveTo(0, y);
+    context.lineTo(600, y);
+    context.stroke();
+  }
 }
 
-for (let y = 20; y <= 600; y += 20) {
-  ctx.beginPath();
-  ctx.fillStyle = "black";
-  ctx.moveTo(0, y);
-  ctx.lineTo(600, y);
-  ctx.stroke();
+
+function dibujarCulebra(context, posX, posY) {
+  context.beginPath();
+  context.fillStyle = "black";
+  context.fillRect(posX, posY, 20, 20);
+  context.stroke();
 }
+
+dibujarCuadricula(ctx);
+
+dibujarCulebra(ctx, 0, 0);
+dibujarCulebra(ctx, 20, 0);
+dibujarCulebra(ctx, 40, 0);
+
+
